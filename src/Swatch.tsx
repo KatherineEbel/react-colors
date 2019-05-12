36470b49197198e68f73494da84b6486cf01aeaf
Katherine Ebel <kathyebel@me.com>
2019-05-12 15:47:02 -0400

    Copy swatch color to clipboard

--- src/Swatch.tsx
+++ src/Swatch.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import CopyToClipboard from 'react-copy-to-clipboard'
 import './Swatch.css'
 
 export interface ISwatchProps {
@@ -8,20 +9,23 @@ export interface ISwatchProps {
 export const Swatch: React.FC<ISwatchProps> = props => {
   const { background, name } = props
   return (
-    <div className="Swatch"
-         style={{ background }}
-    >
-      <div className="Swatch--container">
-        <div className="Swatch--content">
-          <span>{ name }</span>
+    <CopyToClipboard text={ background }>
+      <div className="Swatch"
+           style={{ background }}
+      >
+        <div className="Swatch--container">
+          <div className="Swatch--content">
+            <span>{ name }</span>
+          </div>
+          <button className="Swatch--copy"
+                  type="button"
+          >
+            Copy
+          </button>
+          <span className="Swatch--seeMore">More</span>
         </div>
-        <button className="Swatch--copy"
-                type="button"
-        >
-          Copy
-        </button>
-        <span className="Swatch--seeMore">More</span>
       </div>
-    </div>
+    </CopyToClipboard>
   )
 }
+
