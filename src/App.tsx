ac3687b5e0f18f82f54ef1b6ad843455b51f001e
Katherine Ebel <kathyebel@me.com>
2019-05-13 17:55:23 -0400

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Palette } from './Palette'
 import seedPalettes from './seedColors'
 import { generatePalette } from './colorHelpers'
+import { Route, Switch } from 'react-router'
 
 const App: React.FC = () => {
   const palette = seedPalettes.find(p => p.id === 'flat-ui-colors-aussie')
   return palette ? (
-    <div className="App">
-      <Palette palette={generatePalette(seedPalettes[3])}/>
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette list goes here</h1>}/>
+      <Route exact path="/palette/:id" render={(routeProps) => <h1>Individual palette goes here {`${routeProps.match.params.id}`}</h1>}/>
+    </Switch>
   ) : (
     <div className="App--error">
       <h1>No Palate found!</h1>
