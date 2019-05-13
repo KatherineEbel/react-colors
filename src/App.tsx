8c51b359461154e8624b729c6ed7ca7426d333fa
Katherine Ebel <kathyebel@me.com>
2019-05-13 18:11:08 -0400

    Route to palette by id

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import './App.css';
 import seedPalettes from './seedColors'
 import { generatePalette } from './colorHelpers'
-import { Route, Switch } from 'react-router'
+import { Redirect, Route, Switch } from 'react-router'
+import { Palette } from './Palette'
 
 const App: React.FC = () => {
-  const palette = seedPalettes.find(p => p.id === 'flat-ui-colors-aussie')
-  return palette ? (
+  const matchPalette = (id: string): React.ReactElement => {
+    const palette = seedPalettes.find(p => p.id === id)
+    if (palette) {
+      const chromaPalette = generatePalette (palette)
+      return <Palette palette={chromaPalette}/>
+    }
+    return <Redirect to="/" />
+  }
+
+  return (
     <Switch>
       <Route exact path="/" render={() => <h1>Palette list goes here</h1>}/>
-      <Route exact path="/palette/:id" render={(routeProps) => <h1>Individual palette goes here {`${routeProps.match.params.id}`}</h1>}/>
+      <Route exact path="/palette/:id" render={(routeProps) => matchPalette(routeProps.match.params.id)}/>
     </Switch>
-  ) : (
-    <div className="App--error">
-      <h1>No Palate found!</h1>
-    </div>
   )
 }
 
