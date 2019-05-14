ce83ac79a72b9a9706a36464ba568e7c3730487d
Katherine Ebel <kathyebel@me.com>
2019-05-14 12:12:19 -0400

    Starting styles for MiniPalette

--- src/components/MiniPalette/MiniPalette.tsx
+++ src/components/MiniPalette/MiniPalette.tsx
@@ -1,28 +1,59 @@
 import * as React from 'react'
 import createStyles from '@material-ui/core/styles/createStyles'
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles'
+import { Swatch } from '../../utils/seedColors'
 
 
 // parameter of type import { Theme } from '@material-ui/core/styles/createMuiTheme'
 const styles = () =>
   createStyles({
-    main: {
-      backgroundColor: 'purple',
-      border: '3px solid teal',
-      '& h1': {
-        color: 'white'
+    root: {
+      backgroundColor: 'white',
+      border: '1px solid black',
+      borderRadius: '5px',
+      padding: '0.5rem',
+      position: 'relative',
+      overflow: 'hidden',
+      '&:hover': {
+        cursor: 'pointer'
       }
+    },
+    colors: {
+      backgroundColor: 'grey'
+    },
+    title: {
+      alignItems: 'center',
+      color: 'black',
+      display: 'flex',
+      fontSize: '1rem',
+      justifyContent: 'space-between',
+      margin: '0',
+      paddingTop: '0.5rem',
+      position: 'relative',
+    },
+    emoji: {
+      marginLeft: '0.5rem',
+      fontSize: '1.5rem'
     }
   })
 
 interface Props extends WithStyles<typeof styles> {
-
+  name: string
+  id: string
+  emoji: string
+  colors: Swatch[]
 }
+
 const MiniPalette: React.FC<Props> = (props) => {
-  const { classes } = props
+  {/*<Link to={`/palette/${p.id}`}>{p.name}</Link>*/}
+  const { classes, name, emoji } = props
   return (
-    <div className={ classes.main }>
-      <h1>Mini Palette</h1>
+    <div className={ classes.root }>
+      <div className={ classes.colors }/>
+      <h5 className={ classes.title }>
+        { name }
+        <span className={ classes.emoji }>{ emoji }</span>
+      </h5>
     </div>
   )
 }
--- src/components/PaletteList/PaletteList.tsx
+++ src/components/PaletteList/PaletteList.tsx
@@ -9,12 +9,11 @@ interface IPaletteListProps {
 }
 const PaletteList: React.FC<IPaletteListProps> = ({ palettes }) => {
   const links = palettes.map(p => (
-    <Link to={`/palette/${p.id}`}>{p.name}</Link>
+    <MiniPalette {...p}/>
   ))
   return (
     <div className="PaletteList">
       <h1>React Colors</h1>
-      <MiniPalette/>
       <div className="PaletteList--links">
         <h3>Available palettes:</h3>
         { links }
