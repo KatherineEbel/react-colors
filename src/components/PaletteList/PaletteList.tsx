ca5432eed658b7f7a8ca543deadb462598c2cd90
Katherine Ebel <kathyebel@me.com>
2019-05-14 12:28:06 -0400

    Style PaletteList to display MiniPalettes in grid

--- src/components/PaletteList/PaletteList.tsx
+++ src/components/PaletteList/PaletteList.tsx
@@ -1,25 +1,61 @@
 import * as React from 'react'
 import { IPalette } from '../../utils/seedColors'
-import { Link } from 'react-router-dom'
-import './PaletteList.css'
 import MiniPalette from '../MiniPalette/MiniPalette'
+import createStyles from '@material-ui/core/styles/createStyles'
+import { WithStyles } from '@material-ui/core'
+import withStyles from '@material-ui/core/styles/withStyles'
 
-interface IPaletteListProps {
+const styles = () =>
+  createStyles({
+    root: {
+      alignItems: 'flex-start',
+      backgroundColor: 'blue',
+      display: 'flex',
+      justifyContent: 'center'
+    },
+    container: {
+      alignItems: 'flex-start',
+      display: 'flex',
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+      width: '50%',
+    },
+    miniPalettes: {
+      boxSizing: 'border-box',
+      display: 'grid',
+      gridGap: '5%',
+      gridTemplateColumns: 'repeat(3, 30%)',
+      width: '100%',
+    },
+    nav: {
+      color: 'white',
+      display: 'flex',
+      justifyContent: 'space-between',
+      width: '100%',
+    },
+  })
+
+interface IPaletteListProps extends WithStyles<typeof styles> {
   palettes: IPalette[]
 }
-const PaletteList: React.FC<IPaletteListProps> = ({ palettes }) => {
+
+const PaletteList: React.FC<IPaletteListProps> = ({ classes, palettes }) => {
+  const { root, container, miniPalettes, nav } = classes
   const links = palettes.map(p => (
     <MiniPalette {...p}/>
   ))
   return (
-    <div className="PaletteList">
-      <h1>React Colors</h1>
-      <div className="PaletteList--links">
-        <h3>Available palettes:</h3>
-        { links }
+    <div className={ root }>
+      <div className={ container }>
+        <nav className={ nav }>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={ miniPalettes }>
+          { links }
+        </div>
       </div>
     </div>
   )
 }
 
-export default PaletteList
+export default withStyles(styles)(PaletteList)
