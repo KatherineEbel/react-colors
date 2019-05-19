d6c00ab17ac59bffba73ab83a02d9fd0223e2672
Katherine Ebel <kathyebel@me.com>
2019-05-19 19:32:49 -0400

    Style DraggableSwatch

--- src/components/DraggableSwatch.tsx
+++ src/components/DraggableSwatch.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react'
 import withStyles, { WithStyles } from '@material-ui/styles/withStyles'
 import createStyles from '@material-ui/styles/createStyles'
+import { DeleteRounded } from '@material-ui/icons'
 
 const styles = () =>
   createStyles({
+    delete: {
+      color: 'rgba(0,0,0,0.6)',
+      float: 'right',
+      transition: 'all 0.3s ease-in-out',
+    },
     root: {
       cursor: 'pointer',
       display: 'inline-block',
@@ -11,6 +17,21 @@ const styles = () =>
       margin: '0 auto -3.8px auto',
       position: 'relative',
       width: '20%',
+      '&:hover svg': {
+        color: 'rgba(255,255,255, 0.8)',
+        transform: 'scale(1.5)',
+      },
+    },
+    swatchContent: {
+      bottom: '0',
+      color: '#000',
+      fontSize: '0.7rem',
+      padding: '0.7rem',
+      position: 'absolute',
+      left: 0,
+      letterSpacing: '1px',
+      textTransform: 'uppercase',
+      width: '100%',
     },
   })
 
@@ -21,7 +42,10 @@ interface Props extends WithStyles<typeof styles> {
 const DraggableSwatch: React.FC<Props> = ({ classes, color, name }) => {
   return (
     <li className={classes.root} style={{ background: color }}>
-      {name}
+      <div className={classes.swatchContent}>
+        <span> {name}</span>
+        <DeleteRounded className={classes.delete} />
+      </div>
     </li>
   )
 }
